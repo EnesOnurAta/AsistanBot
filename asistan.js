d440a95e21795133d97d3164d0153ace552d4ea1
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-07-02 16:22:38 +0300

    v2
    Bu güncellemede "Asistan" olarak tanımladığımız yerler sorun çıkmaması açısından "client" olarak düzenlendi.

--- asistan.js
+++ asistan.js
@@ -1,34 +1,32 @@
 const Discord = require("discord.js");
-
-//Asistan, client OLARAK TANITILDI
-const Asistan = new Discord.Client();
+const client = new Discord.Client();
 
 //PREFIX
 var prefix = '=';
 
 //BOT AÇILINCA
-Asistan.on("ready", () => {
+client.on("ready", () => {
   console.log('[------------] Asistan [-------------]');
-  console.log(`${Asistan.guilds.size} tane sunucuya hizmet veriyor`);
-  console.log(`${Asistan.users.size} kullaniciya hizmet veriyor`);
-  console.log(`${Asistan.channels.size} kanala hizmet veriyor`);
+  console.log(`${client.guilds.size} tane sunucuya hizmet veriyor`);
+  console.log(`${client.users.size} kullaniciya hizmet veriyor`);
+  console.log(`${client.channels.size} kanala hizmet veriyor`);
   console.log("Prefix: " + prefix);
-  console.log("Bot ID'si: " + Asistan.user.id);
-  console.log("Bot Isim: " + Asistan.user.username);
+  console.log("Bot ID'si: " + client.user.id);
+  console.log("Bot Isim: " + client.user.username);
   console.log('[------------] Asistan [-------------]');
 });
 
 //Sunucu Botu eklediğinde bot durumunu yayın olarak yeniliyor
-Asistan.on("guildCreate", guild => {
-  Asistan.user.setStatus("STREAMING"); 
+client.on("guildCreate", guild => {
+  client.user.setStatus("STREAMING"); 
 });
-Asistan.on("guildDelete", guild => {
-  Asistan.user.setStatus("STREAMING"); 
+client.on("guildDelete", guild => {
+  client.user.setStatus("STREAMING"); 
 });
 
 //Botu ekleyen sunucu olursa bot kendi sunucusunun belirtilen ID'li kanala log atıyor
-Asistan.on('guildCreate', guild => {
-    let channel = Asistan.channels.get("450419544519999509")
+client.on('guildCreate', guild => {
+    let channel = client.channels.get("450419544519999509")
         const embed = new Discord.RichEmbed()
         .setColor("GREEN")
         .setAuthor(`GIRIS YAPTIM`)
@@ -40,8 +38,8 @@ Asistan.on('guildCreate', guild => {
         .addField("Toplam Kanal", guild.channels.size, true)
          channel.send(embed);
     });
-Asistan.on('guildDelete', guild => {
-    let channel = Asistan.channels.get("450419544519999509")
+client.on('guildDelete', guild => {
+    let channel = client.channels.get("450419544519999509")
         const embed = new Discord.RichEmbed()
         .setColor("RED")
         .setAuthor(`BENI ATTILAR`)
@@ -55,7 +53,7 @@ Asistan.on('guildDelete', guild => {
     });
 
 // SUNUCUYA GİRİŞ
-Asistan.on('guildMemberAdd', member => {
+client.on('guildMemberAdd', member => {
   let Sunucu = member.guild;
   let GirişRolü = guild.roles.find('name', 'Üye');
   member.addRole(GirişRolü);
@@ -71,7 +69,7 @@ Asistan.on('guildMemberAdd', member => {
   GirişKanalı.sendEmbed(GirişMesaj);
 });
 // SUNUCUDAN ÇIKIŞ
-Asistan.on('guildMemberRemove', member => {
+client.on('guildMemberRemove', member => {
   const ÇıkışKanalı = member.guild.channels.find('name', 'çıkış');
   if (!ÇıkışKanalı) return;
   const ÇıkışMesaj = new Discord.RichEmbed()
@@ -83,11 +81,28 @@ Asistan.on('guildMemberRemove', member => {
   ÇıkışKanalı.sendEmbed(ÇıkışMesaj); 
 });
 
-
+const chalk = require('chalk');
+const moment = require('moment');
+const ayarlar = require('../ayarlar.json');
+const snekfetch = require('snekfetch');
+const api = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM1NDIzMTYzMDQwNTA0MjE3NiIsImJvdCI6dHJ1ZSwiaWF0IjoxNTE2Mjc0MTQ1fQ.2H9LjNjH6WFp5LmswfXAYSDsHQn2JSPPgbgf1WjSi_c';
+module.exports = client => {
+  snekfetch.post(`https://discordbots.org/api/bots/${client.user.id}/stats`).set('Authorization', api).send({
+    server_count: client.guilds.size
+  }) console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+  var Games = ["Kendi Cümlenizi Yazın", "Kendi Cümlenizi Yazın", `evo!yardım|${client.guilds.size} sunucu|${client.users.size} kullanıcı`];
+  setInterval(function() {
+    var random = Math.floor(Math.random() * (Games.length - 0 + 1) + 0);
+    client.user.setGame(Games[random], "https://www.twitch.tv/azeriiteammurad");
+  }, 2 * 2500);
+};
 
 
 //DOSYALARI KOMUT OLARAK ALGILAMASI ICIN
-Asistan.on("message", async msg => {
+client.on("message", async msg => {
   if (msg.author.bot) return;
   if(msg.content.indexOf(prefix) !== 0) return;
 
@@ -97,10 +112,10 @@ Asistan.on("message", async msg => {
 
   try {
     let komutdosyası = require(`./komutlar/${komut}.js`);
-    komutdosyası.run(Asistan, msg, argümanlar);
+    komutdosyası.run(client, msg, argümanlar);
   } catch (err) {}
 });
 
 
 //TOKEN
-Asistan.login(process.env.Asistan_TOKEN);
+client.login(process.env.Asistan_TOKEN);
